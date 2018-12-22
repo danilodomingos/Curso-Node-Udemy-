c1d49c890c3f0185d251fc191c24074b5e659d47
Danilo Domingos <danilo.domingos02@gmail.com>
2018-12-22 17:17:17 -0200

    [S3][16] - Adicionando as views do projeto.

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ var app = express();
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res)=>{
-    res.render("index");
+    res.render("home/index");
+});
+
+app.get("/formulario-inclusao-noticia", (req, res)=>{
+    res.render("admin/form_add_noticia");
+});
+
+app.get("/noticias", (req, res)=>{
+    res.render("noticias/noticias");
 });
 
 app.get("/tecnologia", (req, res)=>{
