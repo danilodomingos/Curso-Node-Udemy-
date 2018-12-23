2cb381e54341eb2b554fba4d040ff1ee7214a0d4
Danilo Domingos <danilo.domingos02@gmail.com>
2018-12-23 13:14:55 -0200

    [S3][17] - Adicionando e testando Nodemon.

--- docker_run.sh
+++ docker_run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-docker container run --rm \
+
+if [ "$(docker ps -qaf name=curso_node)" ]; then
+    docker start curso_node && clear;
+    docker exec -it curso_node sh -c "cd /var/app && /bin/sh";
+    docker kill curso_node && clear;
+else
+    docker container run \
     -it \
     -p 3000:3000 \
     -v `pwd`:/var/app \
+    --name curso_node \
     node:10.14.2-alpine \
-    sh -c "cd /var/app && /bin/sh"
+    sh -c "cd /var/app && npm i -g nodemon && /bin/sh"
+fi
