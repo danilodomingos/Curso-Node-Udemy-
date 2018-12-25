0a6ba789ce663a3b751aa144dc9b4ae9771a2fb3
Danilo Domingos <danilo.domingos02@gmail.com>
2018-12-25 19:37:22 -0200

    [S5][21-23] - Seguindo aulas && Configurando o Docker.

--- app.js
+++ app.js
@@ -5,15 +5,7 @@ let rotaHome = require('./app/routes/home')(app);
 let rotaInclusaoNoticias 
     = require('./app/routes/formulario_inclusao_noticia')(app);
 
-
-
-
-
-
-app.get("/tecnologia", (req, res)=>{
-    res.render("secao/tecnologia");
-});
-
+    
 app.listen(3000, () => {
     console.log("Server ON");
 });
