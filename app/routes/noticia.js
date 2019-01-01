53dbe6e9b378659c3d4cd1d28acfd2255523a74d
Danilo Domingos <danilo.domingos02@gmail.com>
2019-01-01 12:45:51 -0200

    [S10][44] - Implementando Express Validator.

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -24,6 +24,21 @@ module.exports = (application) => {
     application.post("/noticias/salvar", (req, res)=>{
         
         let noticia = req.body;
+
+        req.assert('titulo', 'Título é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10,100);
+        req.assert('autor', 'Autor é obrigatório').notEmpty();
+        req.assert('data_noticia', 'Data é obrigatório').notEmpty();
+        req.assert('noticia', 'Notícia é obrigatório').notEmpty();
+
+        let erros = req.validationErrors();
+
+        if(erros){
+            res.render('admin/form_add_noticia')
+            return;
+        }
+
         let connection = application.config.dbConnection();
         let model = new application.app.repository.noticia; 
 
--- config/server.js
+++ config/server.js
@@ -1,18 +1,20 @@
 let express = require('express');
 let consign = require('consign')
 let bodyParser = require('body-parser');
+let expressValidator = require('express-validator');
 
 
 let app = express();
 app.set('view engine', 'ejs');
-app.set('views','./app/views')
+app.set('views','./app/views');
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator());
 
 consign()
     .include('./app/routes')
+    .then('./app/repository')
     .then('config/dbConnection.js')
-    .then('app/repository')
     .into(app);
 
 module.exports = app;
